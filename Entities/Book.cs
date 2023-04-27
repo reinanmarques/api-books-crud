0005ea1841467ac2c13d8179638d5918d1cae482
reinanmarques <wesleremarques@gmail.com>
2023-04-27 15:06:09 -0700

    first commit

---
+++ Entities/Book.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+
+namespace book_api_crud.Entities;
+
+public class Book
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string? Title { get; set; } 
+    public string? Author { get; set; }
+
+    public void Update(string title, string author)
+    {
+        Title = title;
+        Author = author;
+    }
+}
